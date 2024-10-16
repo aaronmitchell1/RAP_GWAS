68de1da97c62bfe7cab1b0b120044f6a2d471030
aaronmitchell1 <112105321+aaronmitchell1@users.noreply.github.com>
2024-10-16 21:56:10 +0100

    Update variant_QC_prep.sh

--- variant_QC_prep.sh
+++ variant_QC_prep.sh
@@ -11,18 +11,13 @@ dx run swiss-army-knife -iin="/Data/diabetes_wes_200k.phe" \
    -icmd="${run_merge}" --tag="Step1" --instance-type "mem1_ssd1_v2_x16"\
    --destination="/Data/" --brief --yes 
 
-#Keep participants from .phe from previous script
-plink2 --bfile ukb_c1-22_merged 
---keep colorectal.phe 
---autosome --maf 0.01 --mac 100 --geno 0.1 --hwe 1e-15 --mind 0.1 --write-snplist --write-samples --no-id-header --out imputed_array_snps_qc_pass
-
-#Then run bgens_qc.wdl on RAP, specify -folder
-
 #Make SNP and ID list from merged chromosome file
 
-plink2 \ 
---bfile ukb_c1-22_hs38DH_merged \ 
---out final_array_snps_qc_pass \ 
---mac 100 --maf 0.01 --hwe 1e-15 --mind 0.1 --geno 0.1 \ 
---write-snplist --write-samples --no-id-header \ 
---threads $(nproc)
+run_plink_qc="plink2 --bfile ukb22418_c1_22_v2_merged --autosome --maf 0.01 --mac 20 --geno 0.1 --hwe 1e-15 --mind 0.1 --write-snplist --write-samples --no-id-header --out snps_qc_pass"
+
+dx run swiss-army-knife -iin="/ukb22418_c1_22_v2_merged.bed" \
+   -iin="/ukb22418_c1_22_v2_merged.bim" \
+   -iin="/ukb22418_c1_22_v2_merged.fam"\
+   -iin="/colorectal.phe" \
+   -icmd="${run_plink_qc}" --tag="Step1" --instance-type "mem1_ssd1_v2_x16"\
+   --destination="/" --brief --yes
