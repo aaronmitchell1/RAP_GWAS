19f8492fe18b5c809c9e60aeb5e1b96f1da40cdf
aaronmitchell1 <112105321+aaronmitchell1@users.noreply.github.com>
2024-10-26 08:42:41 +0100

    Update WES_eQTL_GxE.sh

--- WES_eQTL_GxE.sh
+++ WES_eQTL_GxE.sh
@@ -18,7 +18,8 @@ qnorm((rank(data$bmi,na.last="keep")-0.5)/sum(!is.na(data$bmi)))
 data_regenie$status <- 0
 
 
-#The --interaction is only specified in step 2. Don't specify --catCovarlist if you only have a binary interaction variable or it gives an error.
+#The --interaction is only specified in step 2.
+#You need to specify the interacting variable in covarColList and catCovarList in step 1 then additionally as --interaction in step 2.
 
 run_regenie_step1="regenie --step 1\
  --lowmem --out gxetest_results --bed ukb22418_c1_22_v2_merged\
