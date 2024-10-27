5d4441dd9677cc3574e413ba9bbf70b5de8b7657
aaronmitchell1 <112105321+aaronmitchell1@users.noreply.github.com>
2024-10-27 23:32:28 +0000

    Update GxE_case_status.sh
    fix bsize error (needs to be specified early)

--- GxE/GxE_case_status.sh
+++ GxE/GxE_case_status.sh
@@ -90,12 +90,12 @@ done
 for chr in {1..22}; do
   run_regenie_cmd="regenie --step 2 --out assoc.c${chr} \
      --bed ${data_field}_c${chr}_v3 \
-     --phenoFile combined_qced.phe --covarFile combined_qced.phe \
+     --phenoFile combined_qced.phe --covarFile combined_qced.phe --bsize 200 \
      --phenoCol bmi_int --covarCol age --covarCol batch --covarCol centre \
      --covarCol status --covarCol sex \
      --covarCol pc{1:10} \
      --interaction status
-     --pred GxE_results_pred.list --bsize 200 \
+     --pred GxE_results_pred.list \
      --minMAC 3 --threads 16 --gz"
 
   dx run swiss-army-knife -iin="${data_file_dir}/${data_field}_c${chr}_v3.bed" \
