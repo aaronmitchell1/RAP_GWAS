65dcb7bea6bae40f470272360422e67d1824b37c
aaronmitchell1 <112105321+aaronmitchell1@users.noreply.github.com>
2024-11-12 11:52:34 +0000

    Update prs_bgen_prep.sh

--- PRS/prs_bgen_prep.sh
+++ PRS/prs_bgen_prep.sh
@@ -11,6 +11,18 @@ write.table(scorefile, row.names = FALSE, col.names = TRUE, quote = FALSE, file 
 chrposlist <- paste(GWAS_output$CHROM, paste(GWAS_output$GENPOS, GWAS_output$GENPOS, sep = "-"), sep = ":")
 write.table(chrposlist, row.names = FALSE, col.names = FALSE, quote = FALSE, file = "chrposlist.txt")
 
+#!/bin/bash
+#SBATCH --job-name=bfiles
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=4
+#SBATCH --mail-type=ALL
+#SBATCH --time=47:59:59
+#SBATCH --mail-user=aaron.mitchell@bristol.ac.uk
+#SBATCH --nodes=4
+#SBATCH --account=sscm013902
+#SBATCH --mem=48G
+#SBATCH --partition=cpu
+
 module load gcc/10.5.0
 module load bgen/1.1.7
 
@@ -21,6 +33,6 @@ do
   bgen_file="/mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr${i}.bgen"
   output_file="/user/work/vc23656/GWAS/bfiles_prs/prs_file${i}"
 
-bgenix -g ${bgen_file} -incl-rsids /user/home/vc23656/GWAS/snps_qc_pass.snplist > chr_${i}.bgen
+bgenix -g ${bgen_file} -incl-rsids /user/work/vc23656/GWAS/rsidlist_five_yr_after.txt -incl-range /user/work/vc23656/GWAS/chrposlist_fiveyrsafter.txt > chr_${i}.bgen
 
 done
