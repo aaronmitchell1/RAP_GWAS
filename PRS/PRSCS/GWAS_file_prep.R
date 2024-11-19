7cb059c5df06736ae78e664f47dc52625868f3f0
aaronmitchell1 <112105321+aaronmitchell1@users.noreply.github.com>
2024-11-19 12:39:21 +0000

    Create GWAS_file_prep.R

---
+++ PRS/PRSCS/GWAS_file_prep.R
@@ -0,0 +1,12 @@
+#Convert REGENIE GWAS into PRSCS format
+
+colnames(gwas)[which(names(gwas) == "ID")] <- "SNP"
+colnames(gwas)[which(names(gwas) == "ALLELE1")] <- "A1"
+colnames(gwas)[which(names(gwas) == "ALLELE0")] <- "A2"
+gwas_formatted_PRSCS <- select(gwas, c("SNP", "A1", "A2", "BETA", "SE"))
+
+write.table(gwas_formatted_PRSCS,
+            file = "gwas_formatted_PRSCS.txt",
+            sep = "\t",
+            row.names = FALSE,
+            quote = FALSE)
