38a37e61bfb483c503852e5bcda53a759e09c75f
aaronmitchell1 <112105321+aaronmitchell1@users.noreply.github.com>
2024-11-30 01:25:22 +0000

    Create plink_merge.sh

---
+++ PRS/PLINK/plink_merge.sh
@@ -0,0 +1,3 @@
+out_file="fivetotenyraftermerged.txt"
+files="/user/work/vc23656/PRScs/plink/plink_res_5to10yrafter_c*.bmi_int.glm.linear"
+for f in $files; do     tail -n+2 "$f" | tr " " "\t" | awk -F'\t' '$10 == "ADD"' >> "$out_file"; done
