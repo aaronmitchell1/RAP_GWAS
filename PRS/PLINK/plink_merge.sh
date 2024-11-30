d48c04aa5e8992f2e89c27a41086274b2b00be86
aaronmitchell1 <112105321+aaronmitchell1@users.noreply.github.com>
2024-11-30 10:23:31 +0000

    Update plink_merge.sh

--- PRS/PLINK/plink_merge.sh
+++ PRS/PLINK/plink_merge.sh
@@ -1,4 +1,5 @@
 out_file="fivetotenyraftermerged.txt"
 files="/user/work/vc23656/PRScs/plink/plink_res_5to10yrafter_c*.bmi_int.glm.linear"
-echo -e "CHROM\tPOS\tID\tREF\tALT\tA1\tFIRTH\tTEST\tBETA\tSE\tP" > $out_file
+#copy all output from linear regression PLINK GWAS
+echo -e "CHROM\tPOS\tID\tREF\tALT\tPROVISIONAL_REF?\tA1\tOMITTED\tA1_FREQ\tTEST\tOBS_CT\tBETA\tSE\tT_STAT\tP\tERRCODE" > $out_file
 for f in $files; do     tail -n+2 "$f" | tr " " "\t" | awk -F'\t' '$10 == "ADD"' >> "$out_file"; done
